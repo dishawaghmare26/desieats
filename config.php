75f419258fe5cca11e4af8cddd2ac3a65c445438
dishawaghmare26 <noturacess0226@gmail.com>
2025-09-19 08:56:18 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
-// filepath: c:\Users\Zedex\Desktop\Akshay_excel\1_Style_Caret\Chessfloor\api\config.php
-// Database configuration
+
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root'); // Default XAMPP username
-define('DB_PASSWORD', ''); // Default XAMPP password
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', ''); 
 define('DB_NAME', 'desieats_db');
 
-// Attempt to connect to MySQL database
+
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
+
 if($conn === false){
     die("ERROR: Could not connect. " . $conn->connect_error);
 }
 
-// Start the session
+
 session_start();
+
 ?>
